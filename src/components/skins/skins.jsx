a353aae0949511f72d1eb09f13e4bc5cbce26c36
German <haspenngames14@gmail.com>
2023-03-21 17:24:34 -0300

    fix img src

--- src/components/skins/skins.jsx
+++ src/components/skins/skins.jsx
@@ -1,14 +1,13 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import './skins.css'
+import "./skins.css";
 
 export const Skins = () => {
   const { typeWeapon } = useParams();
   const [skins, setSkins] = useState([]);
   const url = "https://valorant-api.com/v1/weapons/skins";
   const tipoDeArma = typeWeapon;
-  console.log(tipoDeArma)
 
   useEffect(() => {
     axios
@@ -25,16 +24,16 @@ export const Skins = () => {
     return skins.filter((skin) => skin.displayName?.includes(tipoArma));
   }
 
-  console.log(skins);
   return (
     <div className="card-grid">
       {skins?.map((skin) => (
         <div className="card-skin" key={skin.uuid}>
           <p>{skin.displayName}</p>
           <img
-            src={skin.displayIcon ? skin.chromas[0].fullRender : ""}
+            src={skin.chromas[0] ? skin.chromas[0]?.fullRender : skin.chromas[0]?.displayIcon}
             alt={skin.displayName}
           />
+          {console.log(skin.chromas)}
         </div>
       ))}
     </div>
