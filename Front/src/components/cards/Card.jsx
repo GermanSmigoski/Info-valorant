23bc94657a37f6cdf32168beda12d765be15bef1
German <haspenngames14@gmail.com>
2023-05-18 01:13:40 -0300

    css cambios card agents

--- Front/src/components/cards/Card.jsx
+++ Front/src/components/cards/Card.jsx
@@ -1,27 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { getAllAgents } from "../../Redux/Actions";
 import "./Card.css";
 
 export const Card = () => {
+  const [displayBanner, setDisplayBanner] = useState(null);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getAllAgents());
   }, []);
+
+  const showBanner = () => {};
+
   const agents = useSelector((state) => state.agents);
+
   return (
-    <div className="card-grid">
-      {agents.map((agent) => (
-        <div key={agent.uuid} className="card">
-          <div className="card-back">
+    <div className="card-container">
+      <div className="card-grid">
+        {agents.map((agent) => (
+          <div key={agent.uuid} className="card">
+            <img
+              onClick={() => setDisplayBanner(agent)}
+              className="character-img"
+              src={agent?.agentImage}
+              alt=""
+            />
             <Link to={`/personajes/${agent.name}`}>
               <h2 className="card-title-back">{agent.name}</h2>
             </Link>
-            <img className="big-img" src={agent?.agentImage} alt="" />
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      <div className="full-banner">
+        {displayBanner && (
+          <div className="selected-agent">
+            <img src={displayBanner.agentBanner} alt="" />
+            <h2>{displayBanner.name}</h2>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
