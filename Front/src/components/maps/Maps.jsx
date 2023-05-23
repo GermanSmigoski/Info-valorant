cefa85ff36b9c7589f22e47246fe78ab80cc447b
German <haspenngames14@gmail.com>
2023-05-23 10:21:57 -0300

    all maps css

--- Front/src/components/maps/Maps.jsx
+++ Front/src/components/maps/Maps.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllMaps } from "../../Redux/Actions/";
 import "./Maps.css";
@@ -6,24 +6,24 @@ import AllMaps from "./allMaps.jsx";
 import MapCarrousel from "./mapCarrousel.jsx";
 
 export const Maps = () => {
+  const [isActivate, setIsActivate] = useState(false);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getAllMaps());
   }, []);
   const maps = useSelector((state) => state.maps);
+
   return (
-    <div>
-      {maps.map((map) => (
-        <div key={map.id}>
-          <AllMaps
-            id={map.id}
-            name={map.name}
-            image={map.image}
-            icon={map.icon}
-          />
-        </div>
-      ))}
-      <MapCarrousel maps={maps} />
+    <div className="maps-container">
+      <div class='maps-buttons'>
+        <button onClick={() => setIsActivate(false)}>Todas</button>
+        <button onClick={() => setIsActivate(true)}>Una</button>
+      </div>
+      {isActivate && isActivate ? (
+        <MapCarrousel maps={maps} />
+      ) : (
+        <AllMaps maps={maps} />
+      )}
     </div>
   );
 };
--- Front/src/components/maps/allMaps.jsx
+++ Front/src/components/maps/allMaps.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import './allMaps.css'
 
-const AllMaps = ({ id, name, image, icon }) => {
-    return <div></div>;
+const AllMaps = ({ maps }) => {
+  return (
+    <div className="card-grid">
+      {maps.map((map) => (
+        <div className="card" key={map.id}>
+          <div className="card-image">
+            <img src={map.image} alt={`${map.name} image`} />
+          </div>
+          <div className="card-content">
+            <h2>{map.name}</h2>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default AllMaps;
